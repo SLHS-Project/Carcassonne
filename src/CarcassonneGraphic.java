4cf7274d1f40a4f0620c7380371f8d3246e6babe
AG <Asaa1310@gmail.com>
2021-03-22 11:48:43 -0500

    part 1 of gui

---
+++ src/CarcassonneGraphic.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CarcassonneGraphic extends JFrame{
+	
+	public static final int WIDTH = 1920;
+	public static final int HEIGHT = 1080;
+	
+	public CarcassonneGraphic(String frameName) throws IOException
+	{
+		super(frameName);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		add(new CarcassonnePanel());
+		setVisible(true);
+		
+	}
+
+}
---
+++ src/GraphicsRunner.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class GraphicsRunner extends JFrame{
+	
+	public static void main(String args[]) throws IOException
+	{
+		CarcassonneGraphic kd = new CarcassonneGraphic("Carcassonne");
+
+	}
+	
+}
