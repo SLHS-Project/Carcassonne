3468328a7e0fae85bfb78f16908f49a46fcb5c97
SeungheonOh <dan.oh0721@gmail.com>
2021-03-24 12:04:38 -0500

    board rendering done

--- src/CarcassonneMap.java
+++ src/CarcassonneMap.java
@@ -13,7 +13,11 @@
     Rotation is handled at Tile itself.
  */
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class CarcassonneMap {
@@ -23,12 +27,18 @@ public class CarcassonneMap {
         // 0, 0 is left, top
         this.map = new CarcassonneTile[85][85];
         // TODO change this, this mess below is simple start tile for test purposes.
+        BufferedImage timg = null;
+        try {
+            timg = ImageIO.read(CarcassonneTile.class.getResource("/res/tileImg/37.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.map[43][43] = new CarcassonneTile(new Side[] {
                 new Side(TerrainType.Farm, TerrainType.Farm, TerrainType.Farm), //N
                 new Side(TerrainType.Farm, TerrainType.Farm, TerrainType.Farm), //W
                 new Side(TerrainType.Farm, TerrainType.Farm, TerrainType.Farm), //S
                 new Side(TerrainType.Farm, TerrainType.River, TerrainType.Farm)  //E
-        });
+        }, timg);
     }
 
 
@@ -112,8 +122,22 @@ public class CarcassonneMap {
     }
     public BufferedImage render() {
         // get maximum/minimum x and y value
+        Boundary bb = this.getBoundary();
+        int width = bb.x2 - bb.x1 + 1;
+        int height = bb.y2 - bb.y1 + 1;
         // composite
-        return null;
+        // 75 by 75 normally
+        BufferedImage r = new BufferedImage(width * 75, height * 75, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = r.createGraphics();
+        for(int x = bb.x1; x <= bb.x2; x++) {
+            for (int y = bb.y1; y <= bb.y2; y++) {
+                System.out.println(x + " " + y);
+                if(this.map[x][y] == null) continue;
+                g2d.drawImage(this.map[x][y].getImage(), (x - bb.x1) * 75, (y - bb.y1) * 75, null);
+            }
+        }
+        g2d.dispose();
+        return r;
     }
 
     public static void main(String[] args) {
@@ -125,5 +149,12 @@ public class CarcassonneMap {
                 new Side(TerrainType.Farm, TerrainType.Road, TerrainType.Farm)  //E
         }), 44, 43));
         map.getBoundary();
+
+        try {
+            File outputfile = new File("C:\\Users\\k1702639\\Desktop\\a\\map.png");
+            ImageIO.write(map.render(), "png", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
