963aecaea791179187e37c9f04c3046e3ea02666
SeungheonOh <dan.oh0721@gmail.com>
2021-04-23 11:55:10 -0500

    logo fixed

--- src/CarcassonnePanel.java
+++ src/CarcassonnePanel.java
@@ -55,7 +55,7 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         addMouseListener(this);
 
         try {
-            logo = ImageIO.read(CarcassonnePanel.class.getResource("/Images/logo.jpg"));
+            logo = ImageIO.read(CarcassonnePanel.class.getResource("/res/logo.jpg"));
         }
         catch(Exception E) {
             E.printStackTrace();
@@ -89,7 +89,7 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(yellow);
         g.fillRect(getWidth()*20/1920, getHeight()*20/1080, getWidth()-getWidth()*40/1920, getHeight()-getHeight()*40/1080);
-        g.drawImage(logo, getWidth()*1610/1920, getHeight()*20/1080, (getWidth()-getWidth()*25/1920) - (getWidth()*1605/1920), (getHeight()*170/1080) - (getHeight()*20/1080), null);
+        g.drawImage(logo, getWidth()*1610/1920, getHeight()*20/1080, (getWidth()-getWidth()*25/1920) - (getWidth()*1605/1920), (getHeight()*150/1080) - (getHeight()*20/1080), null);
 
         g.setColor(grey);
         g.fillRect(getWidth()*1610/1920, getHeight()*890/1080, (getWidth()-getWidth()*20/1920)-(getWidth()*1610/1920), (getHeight()-getHeight()*20/1080)-(getHeight()*890/1080));
@@ -102,7 +102,7 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         // board location: getWidth()*20/1920, getHeight()*20/1080, getWidth()-getWidth()*25/1920, getHeight()*170/1080
         g.setColor(Color.black);
         g.drawLine(getWidth()*1610/1920, getHeight()*20/1080, getWidth()*1610/1920, getHeight()-getHeight()*25/1080);
-        g.drawLine(getWidth()*1610/1920, getHeight()*170/1080, getWidth()-getWidth()*25/1920, getHeight()*170/1080);
+        g.drawLine(getWidth()*1610/1920, getHeight()*150/1080, getWidth()-getWidth()*25/1920, getHeight()*150/1080);
         g.drawLine(getWidth()*1610/1920, getHeight()*280/1080, getWidth()-getWidth()*25/1920, getHeight()*280/1080);
         g.drawLine(getWidth()*1610/1920, getHeight()*390/1080, getWidth()-getWidth()*25/1920, getHeight()*390/1080);
         g.drawLine(getWidth()*1610/1920, getHeight()*500/1080, getWidth()-getWidth()*25/1920, getHeight()*500/1080);
@@ -116,14 +116,17 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         Font f1 = new Font("Times New Roman", 0, getHeight()*20/1080);
         g.setFont(f1);
         g.setColor(Color.black);
-        g.drawString("Player 1: ", getWidth()*1620/1920, getHeight()*210/1080);
-        g.drawString("Score: ", getWidth()*1620/1920, getHeight()*250/1080);
-        g.drawString("Player 2: ", getWidth()*1620/1920, getHeight()*320/1080);
-        g.drawString("Score: ", getWidth()*1620/1920, getHeight()*360/1080);
-        g.drawString("Player 3: ", getWidth()*1620/1920, getHeight()*430/1080);
-        g.drawString("Score: ", getWidth()*1620/1920, getHeight()*470/1080);
-        g.drawString("Player 4: ", getWidth()*1620/1920, getHeight()*540/1080);
-        g.drawString("Score: ", getWidth()*1620/1920, getHeight()*580/1080);
+        g.drawString("Player 1: " + r.getName(), getWidth()*1620/1920, getHeight()*210/1080);
+        g.drawString("Score: " + r.getScore(), getWidth()*1620/1920, getHeight()*250/1080);
+
+        g.drawString("Player 2: "+ y.getName(), getWidth()*1620/1920, getHeight()*320/1080);
+        g.drawString("Score: " + y.getScore(), getWidth()*1620/1920, getHeight()*360/1080);
+
+        g.drawString("Player 3: "+ b.getName(), getWidth()*1620/1920, getHeight()*430/1080);
+        g.drawString("Score: " + b.getScore(), getWidth()*1620/1920, getHeight()*470/1080);
+
+        g.drawString("Player 4: "+ this.g.getName(), getWidth()*1620/1920, getHeight()*540/1080);
+        g.drawString("Score: " + this.g.getScore(), getWidth()*1620/1920, getHeight()*580/1080);
 
         Font f2 = new Font("Times New Roman", 0, getHeight()*35/1080);
         g.setFont(f2);
@@ -131,7 +134,7 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
 
         Font f3 = new Font("Times New Roman", 0, getHeight()*25/1080);
         g.setFont(f3);
-        g.drawString("Tiles Remaining: ", getWidth()*1625/1920, getHeight()*870/1080);
+        g.drawString("Tiles Remaining: " + (this.dec.size() + this.river.size()), getWidth()*1625/1920, getHeight()*870/1080);
 
         Font f4 = new Font("Times New Roman", 0, getHeight()*30/1080);
         g.setFont(f4);
@@ -219,43 +222,20 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         this.repaint();
     }
 
-    @Override
-    public void mouseEntered(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void mouseExited(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void mousePressed(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void mouseReleased(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
     public void keyPressed(KeyEvent e) {
         if(this.addMeepleState) {
             // Meeple adding here
             switch (e.getKeyChar()) {
-                case 'R':
-                case 'r':
-                    this.statusMessage = "Meeple added to the road";
-                    break;
-                case 'F':
-                case 'f':
-                    this.statusMessage = "Meeple added to the farmland";
-                    break;
-                case 'C':
-                case 'c':
-                    this.statusMessage = "Meeple added to the city";
-                    break;
+                case 'R': case 'r': this.statusMessage = "Meeple added to the road";break;
+                case 'F': case 'f': this.statusMessage = "Meeple added to the farmland";break;
+                case 'C': case 'c': this.statusMessage = "Meeple added to the city";break;
                 case 'N':
                 case 'n':
                     this.statusMessage = "skipped meeple";
-                    break;
+break;
+                default:
+                    this.repaint();
+                    return;
             }
             this.addMeepleState = false;
         } else {
@@ -268,16 +248,11 @@ public class CarcassonnePanel extends JPanel implements MouseListener, ActionLis
         this.repaint();
     }
 
-    @Override
-    public void keyReleased(KeyEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void keyTyped(KeyEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void actionPerformed(ActionEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+    @Override public void keyReleased(KeyEvent arg0) { }
+    @Override public void keyTyped(KeyEvent arg0) { }
+    @Override public void actionPerformed(ActionEvent arg0) { }
+    @Override public void mouseEntered(MouseEvent arg0) { }
+    @Override public void mouseExited(MouseEvent arg0) { }
+    @Override public void mousePressed(MouseEvent arg0) { }
+    @Override public void mouseReleased(MouseEvent arg0) { }
 }
